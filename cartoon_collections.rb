c1c2bfbad8bed3d114de48b33033d95e0a42e06c
cedric e goddard <8ceg6@comcast.net>
2019-10-02 13:30:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,13 +12,13 @@ def summon_captain_planet(veggies)
       end
 end
 
-#def long_planeteer_calls(calls_long)
-  #  answer = false 
-   # long_planeteer_calls.each do |call|
-   #   if call.length > 4
-    #    answer = true 
-   #  end 
-#end
+def long_planeteer_calls(calls_long)
+    answer = false 
+    long_planeteer_calls.each do |call|
+     if call.length > 4
+        answer = true 
+    end 
+end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
